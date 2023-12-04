0f4765319e02d93c14aff92b9ad9478473419b55
kobinarth-panchalingam <kobinarth22@gmail.com>
2023-12-04 09:50:22 +0530

    jdbc template implementation finished

--- src/main/java/gtn/kobi/backend/repository/HobbyRepository.java
+++ src/main/java/gtn/kobi/backend/repository/HobbyRepository.java
@@ -1,12 +1,17 @@
 package gtn.kobi.backend.repository;
 
 import gtn.kobi.backend.model.Hobby;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface HobbyRepository extends JpaRepository<Hobby, Integer> {
+public interface HobbyRepository {
     List<Hobby> findByUserId(Integer userId);
+    Hobby save(Hobby hobby);
+
+    Optional<Hobby> findById(Integer id);
+
+    void delete (Hobby hobby);
 }
--- src/main/java/gtn/kobi/backend/repository/UserRepository.java
+++ src/main/java/gtn/kobi/backend/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package gtn.kobi.backend.repository;
 
 import gtn.kobi.backend.model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Integer> {
+public interface UserRepository {
     Optional<Users> findByUsername(String username);
+    Users save(Users user);
 }
